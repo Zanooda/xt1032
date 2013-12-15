7a1f56b60d5a56f38c938262c02e87032d5a877d
Daniel Storozhev <daniel.storozhev@gmail.com>
2013-12-15 16:25:44 +0100

    F2FS fix

--- device_xt1032.mk
+++ device_xt1032.mk
@@ -15,8 +15,9 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
+    $(LOCAL_KERNEL):kernel \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom \
+    $(LOCAL_PATH)/init.qcom.rc:root/init.qcom.rc
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
